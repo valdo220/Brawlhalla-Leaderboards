2b5d14f5577759a665666914f144672ebd7b9393
Valdo <oswaldojr2200@gmail.com>
2025-04-04 23:15:57 -0500

    brackets now updating

--- main.js
+++ main.js
@@ -7,22 +7,28 @@ const numberIcon = document.querySelectorAll(".number-icon")
 const previousButtons = document.querySelectorAll(".previous-button")
 const selectOne = document.querySelectorAll(".select-one")
 const selectTwo = document.querySelectorAll(".select-two")
+const player1 = document.querySelector(".player1")
+const player2 = document.querySelector(".player2")
+const propertiesInsideLeaderboard = document.querySelector(".properties-inside-leaderboard")
+const propertiesOfLeaderboard = document.querySelector(".properties-of-leaderboard")
 let currentCatagoryOne = null
 let currentCatagoryTwo = null
-console.log(currentCatagoryOne)
 
 async function myApp() {
     let page = 1
     let region = "all"
     let bracket = "1v1"
     let pageNumber = 1
+    let brawhalla_id
 
-    console.log(selectOne)
-    console.log(selectTwo)
     let rankingsAsyncAwait = null
     const response = await fetch(`${BASE_URL}/rankings/${bracket}/${region}/${page}?&api_key=${API_KEY}`)
     const data = await response.json()
     rankingsAsyncAwait = data
+    let playerIdAsyncAwait = null
+    const responsePlayer = await fetch(`${BASE_URL}/player/${brawhalla_id}/stats?&api_key=${API_KEY}`)
+    const dataPlayer  =  await responsePlayer.json()
+    playerIdAsyncAwait = dataPlayer
 
     for (let i = 0; i < data.length; i++) {
         let clone = original.cloneNode(true)
@@ -59,6 +65,26 @@ async function myApp() {
             }
         }
     }
+
+    async function switchingHeader() {
+        if (bracket === "1v1") {
+            player2.style.display = "none"
+            player1.textContent = "name"
+            propertiesInsideLeaderboard.style.gridTemplateColumns = "3rem 5rem 5rem auto 5rem 8rem 6rem 6rem"
+            propertiesOfLeaderboard.style.gridTemplateColumns = "3rem 5rem 5rem auto 5rem 8rem 6rem 6rem"
+        }
+        if (bracket === "2v2") {
+            player2.style.display = "block"
+            player1.textContent = "player 1"
+            propertiesInsideLeaderboard.style.gridTemplateColumns = "3rem 5rem 5rem auto auto 5rem 8rem 6rem 6rem"
+            propertiesOfLeaderboard.style.gridTemplateColumns = "3rem 5rem 5rem auto auto 5rem 8rem 6rem 6rem"
+        }
+    }
+
+    async function switchingBrackets(selectNumber) {
+        bracket = selectNumber.textContent
+        }
+
     function grabCatagory(selectNumber) {
         selectNumber.classList.add("blue");
         return selectNumber
@@ -82,7 +108,11 @@ async function myApp() {
         async function updateLeaderboard(page) {
             const response = await fetch(`${BASE_URL}/rankings/${bracket}/${region}/${page}?&api_key=${API_KEY}`)
             const data = await response.json()
-
+            if (bracket === "2v2") {
+                const responsePlayer = await fetch(`${BASE_URL}/player/${brawhalla_id}/stats?&api_key=${API_KEY}`)
+                const dataPlayer  =  await responsePlayer.json()
+            }
+                
             let allClones = await getClones(data.length)
             for (let i = 0; i < allClones.length; i++) {
                 let losses = data[i].games - data[i].wins
@@ -100,6 +130,10 @@ async function myApp() {
                 allClones[i].querySelector('.winrate').textContent = winRate
                 allClones[i].querySelector('.elo').innerHTML = data[i].rating +
                     `<span style="color: gray; font-size: 12px;"> / ${data[i].peak_rating}</span>`
+                    if (bracket === "2v2") {
+                        allClones[i].querySelector('.player1').textContent = data[i].brawlhalla_id_one
+                        allClones[i].querySelector('.player2').textContent = data[i].brawlhalla_id_two
+                    }
             }
         }
 
@@ -139,13 +173,22 @@ async function myApp() {
     selectOne[0].addEventListener('click', async() => {
         removeBlueFromPrev(selectOne)
         currentCatagoryOne = grabCatagory(selectOne[0])
+        switchingBrackets(selectOne[0])
+        switchingHeader()
+        updateLeaderboard(page)
+        console.log(data)
         console.log(currentCatagoryOne)
-        console.log(region)
+        console.log(bracket)
     })
     selectOne[1].addEventListener('click', () => {
         removeBlueFromPrev(selectOne)
         currentCatagoryOne = grabCatagory(selectOne[1])
+        switchingBrackets(selectOne[1])
+        switchingHeader()
+        updateLeaderboard(page)
+        console.log(data)
         console.log(currentCatagoryOne)
+        console.log(bracket)
     })
     selectTwo[0].addEventListener('click', () => {
         removeBlueFromPrev(selectTwo)
